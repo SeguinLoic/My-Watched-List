ac6ef97b3fd3937f084e6762220aaebcbc3061e8
Loïc Seguin <loicseguin74@gmail.com>
2021-01-09 17:47:19 +0100

    Add TMDB attribution

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import dame from "./images/dame.jpg"
 import mandalorian from "./images/mandalorian.jpg"
 import vikings from "./images/vikings.jpg"
 import umbrella from "./images/umbrella.jpg"
+import logoTMDB from "./images/logo-tmdb.svg";
 
 function App() {
   return (
@@ -32,6 +33,11 @@ function App() {
         <img src={dame} alt="dame"/>
         <img src={mandalorian} alt="mandalorian"/>
         <img src={umbrella} alt="umbrella"/>
+      </div>
+
+      <div className="attribution">
+        <span>This product uses the TMDb API but is not endorsed or certified by TMDb.</span>
+        <img src={logoTMDB} />
       </div>
     </div>
   );
