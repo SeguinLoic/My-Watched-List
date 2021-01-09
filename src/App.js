43715790225d0f76901fa85b500960c98a0567bf
Loïc Seguin <loicseguin74@gmail.com>
2021-01-09 19:19:23 +0100

    API REQUEST OK

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 
 import got from "./images/got.jpg"
@@ -9,15 +9,31 @@ import umbrella from "./images/umbrella.jpg"
 import logoTMDB from "./images/logo-tmdb.svg";
 
 function App() {
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${search}`);
+    const myJSON = await response.json();
+    setResult(myJSON.results);
+  }
+
   return (
     <div className="App">
       <header>
         <h1>My series</h1>
-        <form>
-          <input type="text"/>
+        <form onSubmit={handleSearch}>
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
           <button>Search</button>
         </form>
       </header>
+
+      <h2>Ma recherche</h2>
+      <div className="search">
+      </div>
 
       <h2>Les dernières sorties</h2>
       <div className="sorties">
@@ -28,16 +44,16 @@ function App() {
         <img src={umbrella} alt="umbrella"/>
       </div>
 
-      <h2>Mon top</h2>
+      {/*<h2>Mon top</h2>
       <div className="monTop">
         <img src={dame} alt="dame"/>
         <img src={mandalorian} alt="mandalorian"/>
         <img src={umbrella} alt="umbrella"/>
-      </div>
+      </div>*/}
 
       <div className="attribution">
         <span>This product uses the TMDb API but is not endorsed or certified by TMDb.</span>
-        <img src={logoTMDB} />
+        <img src={logoTMDB} alt="Logo TMDB" />
       </div>
     </div>
   );
