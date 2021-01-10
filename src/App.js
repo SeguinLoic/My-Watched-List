d10dc7db83dc57b3b24a404dae30663664728e70
Loïc Seguin <loicseguin74@gmail.com>
2021-01-10 16:20:55 +0100

    Creation of 'My List' state

--- src/App.js
+++ src/App.js
@@ -13,43 +13,77 @@ function App() {
   const API_KEY = process.env.REACT_APP_API_KEY;
   const [search, setSearch] = useState("");
   const [result, setResult] = useState([]);
+  const [myList, setMyList] = useState([]);
+
 
   const handleSearch = async (e) => {
     e.preventDefault();
     const response = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${search}`);
     const myJSON = await response.json();
     setResult(myJSON.results);
+    setSearch("");
   }
 
   return (
     <div className="App">
       <header>
-        <h1>My series</h1>
+        <h1>My Watched List</h1>
         <form onSubmit={handleSearch}>
           <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
           <button>Search</button>
         </form>
       </header>
 
-      <h2>Ma recherche</h2>
+
       <div className="search">
+        {
+          result.length 
+          ? 
+          <div className="blocSearch">
+            <h2>Ma recherche</h2>
+            <div className="result">
+              {
+                result.map(res => {
+                  return (
+                    <div>
+                      <img src={`https://image.tmdb.org/t/p/w500${res.poster_path}`} alt="hey"/>
+                      {res.original_name}
+                      <button onClick={() => setMyList([...myList, {res, note: ""}])}>Ajouter à ma liste</button>
+                    </div>
+                  )
+                })
+              } 
+            </div>
+          </div>
+          : 
+            <div className="blocSortie">
+              <h2>Les dernières sorties</h2>
+              <div className="sorties">
+                <img src={got} alt="got"/>
+                <img src={dame} alt="dame"/>
+                <img src={mandalorian} alt="mandalorian"/>
+                <img src={vikings} alt="vikings"/>
+                <img src={umbrella} alt="umbrella"/>
+              </div>
+            </div>
+
+        }
       </div>
 
-      <h2>Les dernières sorties</h2>
-      <div className="sorties">
-        <img src={got} alt="got"/>
-        <img src={dame} alt="dame"/>
-        <img src={mandalorian} alt="mandalorian"/>
-        <img src={vikings} alt="vikings"/>
-        <img src={umbrella} alt="umbrella"/>
-      </div>
-
-      {/*<h2>Mon top</h2>
+      <h2>Mon top</h2>
       <div className="monTop">
-        <img src={dame} alt="dame"/>
-        <img src={mandalorian} alt="mandalorian"/>
-        <img src={umbrella} alt="umbrella"/>
-      </div>*/}
+        {
+          myList.length 
+          ? myList.map(serie => <div><img src={`https://image.tmdb.org/t/p/w500${serie.res.poster_path}`} alt="hey" /><span>{serie.res.original_name}</span></div>)
+          : (
+            <>
+              <img src={dame} alt="dame"/>
+              <img src={mandalorian} alt="mandalorian"/>
+              <img src={umbrella} alt="umbrella"/>
+            </>
+          )
+        }
+      </div>
 
       <div className="attribution">
         <span>This product uses the TMDb API but is not endorsed or certified by TMDb.</span>
