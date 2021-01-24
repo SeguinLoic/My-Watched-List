e368f8dc78ee695f214dc13444702718a8d5a819
Loïc Seguin <loicseguin74@gmail.com>
2021-01-24 18:31:43 +0100

    Conditional Navigation

--- src/components/Navigation/LogNavigation.js
+++ src/components/Navigation/LogNavigation.js
@@ -1,12 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+import "./LogNavigation.css";
 
 export default function LogNavigation() {
+
+    const location = useLocation();
+    const [currentLocation, setCurrentLocation] = useState("");
+
+    useEffect(() => {
+        console.log(location.pathname);
+        setCurrentLocation(location.pathname);
+    }, [location.pathname])
+
     return (
         <div className="logNavigation">
-                <Link to="/Login">Déjà membre ?</Link>
-                <Link to="/Signin" >Pas inscrit ?</Link>
-                <Link to="/ForgotPassword">Mot de passe oublié ?</Link>
+                <Link to="/Login" className={currentLocation === "/Login" ? "hide" : ""}>Déjà membre ?</Link>
+                <Link to="/Signin" className={currentLocation === "/Signin" ? "hide" : ""}>Pas inscrit ?</Link>
+                <Link to="/ForgotPassword" className={currentLocation === "/ForgotPassword" ? "hide" : ""}>Mot de passe oublié ?</Link>
         </div>
     )
 }
