c868bd509bc7a3e835fe5cb90e3e3da511d53bc2
Loïc Seguin <loicseguin74@gmail.com>
2021-03-10 07:58:52 +0100

    MoviePage Style

--- src/ui/pages/Movie/SeriePage.js
+++ src/ui/pages/Movie/SeriePage.js
@@ -1,18 +1,30 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
+import "./SeriePage.css"
+
 export default function SeriePage() {
 
     const location = useLocation();
 
     return (
-        <div className="seriePage">
-            <img src={`https://image.tmdb.org/t/p/w1280${location.state.serie.backgroundImage}`} alt="hey"/>
-            <img src={`https://image.tmdb.org/t/p/w500${location.state.serie.presentationImage}`} alt="hey"/>
-            <h1>{location.state.serie.movieName}</h1>
-            <span>Date : {location.state.serie.movieDate}</span>
-            <span>Résumé : {location.state.serie.movieOverview}</span>
-            <span>Note : {location.state.serie.movieRate}</span>
+        <div className="page">
+            <div className="headerSerie">
+                <span className="background">
+                    <img src={`https://image.tmdb.org/t/p/original${location.state.serie.backgroundImage}`} className="backgroundHeader"  alt="hey"/>
+                </span>
+                <div className="headerContent">
+                    <img src={`https://image.tmdb.org/t/p/w500${location.state.serie.presentationImage}`} alt="hey"/>
+                    <div className="infoHeader">
+                        <h1>{location.state.serie.movieName}</h1>
+                        <span>{location.state.serie.movieRate} / 10</span>
+                    </div>
+                </div>
+            </div>
+            <div className="contentSerie">
+                <span className="date">{location.state.serie.movieDate}</span>
+                <span className="resume">{location.state.serie.movieOverview}</span>
+            </div>
             <Link to={`${location.state.location}`}>Retour</Link>
         </div>
     )
